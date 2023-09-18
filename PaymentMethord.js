e8e92960a3cf8a0e845a98c9a6d0a5dd844cca16
Alsaef <110528853+Alsaef@users.noreply.github.com>
2023-09-18 20:07:31 +0600

    Update PaymentMethord.js

--- PaymentMethord.js
+++ PaymentMethord.js
@@ -1,13 +1,20 @@
  // payment methord
-    app.post("/create-payment-intent",verify,async(req,res)=>{
-       const {price}=req.body;
-       const ammount=price*100;
-       const paymentIntent= await stripe.paymentIntents.create({
-         ammount:ammount,
-         currency: "usd",
-         payment_method_types:["card"]
-       });
-       res.send({
-        clientSecret: paymentIntent.client_secret,
-       })
-    })
+ app.post('/create-payment-intent', verify, async (req, res) => {
+      const { price } = req.body;
+      const amount = price * 100; // Convert price to cents (Stripe expects the amount in cents)
+      
+      try {
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: 'usd',
+          payment_method_types: ['card']
+        });
+    
+        res.send({
+          clientSecret: paymentIntent.client_secret
+        });
+      } catch (error) {
+        console.error('Error creating payment intent:', error);
+        res.status(500).send({ error: 'Error creating payment intent' });
+      }
+    }); 
