1c22f0f0b71a45e9a7d60f56debee77c94707c65
Alsaef <110528853+Alsaef@users.noreply.github.com>
2023-10-18 20:06:13 +0600

    Create mongoes_ImageUrl.js

---
+++ mongoes_ImageUrl.js
@@ -0,0 +1,19 @@
+ imageUrls:{
+     type:String,
+     require:true,
+     validate:{
+      validator:(value)=>{
+        if (!Array.isArray) {
+            return false
+        }
+        let isValid=true
+        value.forEach(url => {
+            if (!validator.isURL(url)) {
+              isValid= false
+            } 
+        });
+        return isValid=true
+      },
+      message:'validate url'
+     }
+    },
