4168d32b7106d29baa1654949f71c01455c77637
Al Saef Ratul <110528853+Alsaef@users.noreply.github.com>
2025-06-01 12:23:43 +0600

    Create CookiescookieParser.js

---
+++ latest-security/CookiescookieParser.js
@@ -0,0 +1,59 @@
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
+// middleware
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
+
+// use verify middleware
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token; // cookie থেকে token নেওয়া হচ্ছে
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+    req.decoded = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+};
+
+  // jwt token related api
+    app.post('/jwt', async (req, res) => {
+      const userInfo = req.body;
+
+      const token = jwt.sign(userInfo, process.env.JWT_ACCESS_SECRET, { expiresIn: '2h' });
+
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: false
+      })
+
+      res.send({ success: true })
+
+    })
+
+// optional
+
+const verifyEmailMatch = (req, res, next) => {
+  const emailFromQuery = req.query.email;
+  const emailFromToken = req.tokenEmail; // এটা ধরে নিচ্ছি তুমি আগে JWT verify middleware এ set করছো
+
+  if (emailFromToken !== emailFromQuery) {
+    return res.status(403).send({ message: 'forbidden access' });
+  }
+
+  next();
+};
+
+
+
+
+// secure api
