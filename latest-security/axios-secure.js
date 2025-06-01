5cd583472e9566362d9379fe19394fe291da1507
Al Saef Ratul <110528853+Alsaef@users.noreply.github.com>
2025-06-01 12:38:01 +0600

    Create axios-secure.js

---
+++ latest-security/axios-secure.js
@@ -0,0 +1,41 @@
+if you use firebase and full code seperate
+
+import axios from 'axios';
+import React from 'react';
+// your file  path import
+import useAuth from './useAuth';
+
+const axiosInstance = axios.create({
+    baseURL: 'https://example' // your api implement
+})
+
+const useAxiosSecure = () => {
+  // if your function implement signOutUser || logOut
+    const { user, signOutUser } = useAuth();
+
+    axiosInstance.interceptors.request.use(config => {
+        config.headers.authorization = `Bearer ${user.accessToken}`
+        return config;
+    });
+
+    // response interceptor
+    axiosInstance.interceptors.response.use(response => {
+        return response;
+    }, error => {
+        console.log(error)
+        if (error.status === 401 || error.status === 403) {
+            signOutUser()
+                .then(() => {
+                    console.log('sign out user for 401 status code')
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+        return Promise.reject(error)
+    })
+
+    return axiosInstance;
+};
+
+export default useAxiosSecure;
